2bac9752932bb69fec37946fac1c430d0281430a
Andrei <blue.screen@inbox.ru>
2022-01-21 17:13:20 +0300

    GeekShop_opt_srv

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zv%5hj%j%abww1ig!9xuyh^nd(9ule$c_%avr2l%7)l3xbn0f#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -84,10 +84,19 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
+
     }
 }
 
@@ -165,8 +174,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 
-SOCIAL_AUTH_VK_OAUTH2_KEY = '8043026'
-SOCIAL_AUTH_VK_OAUTH2_SECRET = 'fYXDGVfGIvdT0Pqe8zNg'
+SOCIAL_AUTH_VK_OAUTH2_KEY = ''
+SOCIAL_AUTH_VK_OAUTH2_SECRET = ''
 
 # SOCIAL_AUTH_VK_OAUTH2_KEY = VK['8043026']
 # SOCIAL_AUTH_VK_OAUTH2_SECRET = VK['fYXDGVfGIvdT0Pqe8zNg']
